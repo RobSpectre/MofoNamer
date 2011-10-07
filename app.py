c5e0a22797920373f66f746a61c8d5ae99b93319
Rob Spectre <rob.spectre@gmail.com>
2011-10-07 16:06:03 -0400

    Added skeleton of templating.

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 import os
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index(page_title="Home"):
+    return render_template('index.html', page_title=page_title)
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
+    # Disable in production
+    app.debug = True
     app.run(host='0.0.0.0', port=port)
