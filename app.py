1c2a3697c049a3a525e6a90209c91a2c2b4736d7
Kyle Conroy <kyle.j.conroy@gmail.com>
2011-10-07 16:33:58 -0400

    Remove WTForms

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ from flask import render_template
 from flask import request
 from flask import url_for
 from flask import redirect
-from forms import NameForm
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from settings import username, password
@@ -38,13 +37,12 @@ app = Flask(__name__, static_url_path='/static')
 
 
 @app.route('/', methods=['GET', 'POST'])
-def register():
-    form = NameForm(request.form)
-    if request.method == 'POST': #and form.validate():
+def index(page_title="Home"):
+    if request.method == 'POST': 
         flash('Thanks for registering')
         send_email("kyle.j.conroy@gmail.com", "kyle@twilio.com", 3)
         return redirect(url_for('registered'))
-    return render_template('signup.html', form=form)
+    return render_template('index.html', page_title=page_title)
 
 
 @app.route('/registered')
@@ -58,14 +56,4 @@ if __name__ == '__main__':
     if port == 5000:
         app.debug = True
 
-=======
-@app.route('/')
-def index(page_title="Home"):
-    return render_template('index.html', page_title=page_title)
-
-if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    # Disable in production
-    app.debug = True
->>>>>>> 7a66f97b7f7c523f7339de18cea8c75cb1ecc16a
     app.run(host='0.0.0.0', port=port)
